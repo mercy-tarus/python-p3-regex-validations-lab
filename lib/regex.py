ff14dad832a7fb873987032bb1aaaff8686e23f1
mercy-tarus <merciechelah1@gmail.com>
2024-06-09 12:11:17 +0300

    complete

--- lib/regex.py
+++ lib/regex.py
@@ -5,9 +5,12 @@ import re
 
 name = r""
 name_regex = re.compile(name)
+name_regex = re.compile(r"^[A-Z][a-zA-Z]*([ '-][A-Z][a-zA-Z]*)*$")
 
 phone_number = r""
 phone_regex = re.compile(phone_number)
+phone_regex = re.compile(r"^(\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$")
 
 email_address = r""
 email_regex = re.compile(email_address)
+email_regex = re.compile(r"^[a-zA-Z][a-zA-Z0-9.]*@[a-zA-Z]+\.[a-zA-Z]{2,}$")
